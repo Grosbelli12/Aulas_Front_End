391b0ed1d52fc04be8c6747c7fb8f6bc61b62962
Grosbelli12 <gustavogrosbelli11@gmail.com>
2025-08-14 22:36:03 -0300

    componentes treinados

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Unipar from './Unipar.jsx'
+import Rodrigo from './Rodrigo.jsx'
+
+function App() {
+
+  return (
+    <> {/*fragmento para rodar um bloco*/}
+    <div>
+      <Unipar />
+      <Rodrigo/>
+      <p>Ele tem 190 anos e ainda esta na flor da idade</p>
+    </div>
+    </>
+  )
+}
+
+export default App;
