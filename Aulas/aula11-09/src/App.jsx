b100d626544e82e32fa1cd5ee5d49262f69a1ecc
Grosbelli12 <gustavogrosbelli11@gmail.com>
2025-09-15 20:44:07 -0300

    coisas adicionadas

---
+++ Aulas/aula11-09/src/App.jsx
@@ -0,0 +1,94 @@
+import { use, useEffect, useState } from "react";
+import "./App.css";
+
+// Quando tenho apenas uma linha no arrow function não preciso usar {}
+// map funciona igual um for
+// ... separa os atributos como id, nome e pega os valores e entrega o que vc precisa
+
+function App() {
+  const [tarefa, setTarefa] = useState("");
+  const [listaTarefas, setListaTarefas] = useState([]);
+
+  useEffect(() => {
+    setListaTarefas(JSON.parse(localStorage.getItem("LISTA_TAREFA")));
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("LISTA_TAREFA", JSON.stringify(listaTarefas));
+  }, [listaTarefas])
+
+  const adicionarTarefa = () => {
+    if (tarefa.trim() === "") {
+      alert("Informe uma tarefa");
+      return;
+    }
+    const novaTarefa = {
+      id: Date.now(),
+      nome: tarefa,
+      completada: false,
+    };
+    setListaTarefas([...listaTarefas, novaTarefa]);
+    setTarefa("");
+  };
+
+  const excluirTarefa = (id) => {
+    setListaTarefas(listaTarefas.filter((task) => task.id != id));
+  };
+
+  const alterarSituacao = (id) => {
+    setListaTarefas(
+      listaTarefas.map((task) =>
+        task.id == id ? { ...task, completada: !task.completada } : task
+      )
+    );
+  };
+  return (
+    <div className="todo-container">
+      <h2>Lista de Tarefas ✅</h2>
+
+      <p>Total tarefas : {listaTarefas.length || "Sem Tarefas"}</p>
+
+      <div className="input-container">
+        <input
+          type="text"
+          value={tarefa}
+          onChange={(e) => setTarefa(e.target.value)}
+          placeholder="Digite uma tarefa"
+        />
+
+        <button onClick={adicionarTarefa}>Adicionar</button>
+      </div>
+
+      <ul>
+        {listaTarefas.map((task) => (
+          <li
+            key={task.id}
+            style={{
+              textDecoration: task.completada ? "line-through" : "none",
+              color: task.completada ? "gray" : "black",
+            }}
+          >
+            {task.nome}
+            <div>
+              <button
+                onClick={() => alterarSituacao(task.id)}
+                className="complete-btn"
+              >
+                ✔
+              </button>
+              <button
+                disabled={task.completada}
+                onClick={() => excluirTarefa(task.id)}
+                className="delete-btn"
+              >
+                ❌
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
