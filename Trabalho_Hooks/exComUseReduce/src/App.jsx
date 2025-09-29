7c55faa76057b10bf46989a46270dc15d8fd88f7
Grosbelli12 <gustavogrosbelli11@gmail.com>
2025-09-29 20:21:27 -0300

    css atualizados

--- Trabalho_Hooks/exComUseReduce/src/App.jsx
+++ Trabalho_Hooks/exComUseReduce/src/App.jsx
@@ -1,23 +1,22 @@
 import { useReducer } from "react";
 import "./App.css";
-import ExComUseState from "./ExComUseState";
 
 // useReducer tem como sua principal função tornar mais fácil gerir estados complexos de forma centralizada.
 // com ele você pode criar várias variáveis de estado para manipular, você consegue passar ações para ele também.
 // o UseReducer começa com a função reducer que você cria fora dele, que será onde você vai receber uma ação e irá devolver um novo estado
 // o state é a variavel que guarda o valor atual do estado, no ínicio será igual ao que for definido, no caso do exemplo é 0
 // eu uso o state.contador para acessar ela e fazer a leitura dela sem modificar diretamente
-// dispatch é a função que você usa para iniciar a mudança de estado, você dispacha uma ordem, no caso do exemplo é  () => dispatch({ type: 'INCREMENTAR' })
+// dispatch é a função que você usa para iniciar a mudança de estado, você dispacha uma ordem, no caso do exemplo é () => dispatch({ type: 'INCREMENTAR' })
 // e entrega essa ordem a função reducer que tem o parâmetro action que recebe essa ordem
 // com o estado passado e a ordem também a função reducer faz o calculo/função definida e retorna um novo valor de estado
-// a grande diferença do useReducer para o useState é essa o poder de eu fazer várias ações em vários estados em uma única função 
+// a grande diferença do useReducer para o useState é essa: o poder de eu fazer várias ações em vários estados em uma única função 
 
 function reducer(state, action) {
   console.log("Reducer foi chamado com a ação:", action.type);
   switch (action.type) {
-    case "INCREMENTAR":
+    case "AUMENTAR":
       return { ...state, contador: state.contador + 1 };
-    case "DECREMENTAR":
+    case "DIMINUIR":
       return { ...state, contador: state.contador - 1 };
     case "RESETAR":
       return {...state, contador: state.contador = 0};
@@ -30,18 +29,21 @@ function App() {
   const [state, dispatch] = useReducer(reducer, { contador: 0 });
 
   return (
-    
-    <div>
+    <div className="caixa">
       <h2>Contador com useReducer</h2>
       <h1>{state.contador}</h1>
 
-      <button onClick={() => dispatch({ type: "INCREMENTAR" })}>
-        Incrementar +
-      </button>
-      <button onClick={() => dispatch({ type: "DECREMENTAR" })}>
-        Decrementar -
-      </button>
-      <button onClick={() => dispatch({ type: "RESETAR" })}>Resetar</button>
+      <div className="botoes">
+        <button onClick={() => dispatch({ type: "AUMENTAR" })}>
+          Aumentar +
+        </button>
+        <button onClick={() => dispatch({ type: "DIMINUIR" })}>
+          Diminuir -
+        </button>
+        <button className="reset" onClick={() => dispatch({ type: "RESETAR" })}>
+          Resetar
+        </button>
+      </div>
     </div>
   );
 }
--- Trabalho_Hooks/exComUseRef/src/App.jsx
+++ Trabalho_Hooks/exComUseRef/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useRef } from "react";
+import "./ContadorCliques.css";
+
+/* 
+  Neste exemplo o useRef "cliques" irá começar com 0 e quando o botão "Contar" for pressionado,
+  o valor presente em ".current" será incremetado e um console.log executado.
+
+  Na tela o valor atualizado não será exibido, isto somente ocorrerá quando o botão "Forçar Render"
+  for pressionado.
+
+  Esse botão atualiza o estado do state "render", ao fazer isso, uma re-renderização acontece, pois
+  o useState sempre re-renderiza a tela.
+*/
 
 function App() {
-  const [count, setCount] = useState(0)
+  const cliques = useRef(0);
+  const [render, setRender] = useState(false);
+
+  function contar() {
+    cliques.current += 1;
+    console.log("Cliques:", cliques.current);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="caixa">
+      <button onClick={contar}>Contar</button>
+      <button onClick={() => setRender(!render)}>Forçar render</button>
+      <p>Cliques: {cliques.current}</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
